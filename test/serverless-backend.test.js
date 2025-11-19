a4859dabd46fe81bc22ba75985857cb5e786695d
FakeGods <w.leszkowicz99@gmail.com>
2025-11-19 22:24:47 +0100

    Initial commit

---
+++ test/serverless-backend.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ServerlessBackend = require('../lib/serverless-backend-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-backend-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ServerlessBackend.ServerlessBackendStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
