8987a9dc716ddf20104359e4dc7075930b4e990a
FakeGods <w.leszkowicz99@gmail.com>
2025-11-19 22:34:52 +0100

    settng env

--- bin/serverless-backend.js
+++ bin/serverless-backend.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+
 const cdk = require('aws-cdk-lib');
 const { ServerlessBackendStack } = require('../lib/serverless-backend-stack');
 
@@ -15,7 +16,7 @@ new ServerlessBackendStack(app, 'ServerlessBackendStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+   env: { account: '695438154048', region: 'eu-central-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
